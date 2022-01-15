77218e84c33dc2f7173349733db317098ee752c2
Shalitha Suranga <shalithasuranga@gmail.com>
2022-01-15 14:00:10 +0530

    Use final instead of late via initializer set

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -1,15 +1,14 @@
 class Book {
-  late int id;
-  late String title;
-  late int year;
+  final int id;
+  final String title;
+  final int year;
 
   Book(this.id, this.title, this.year);
 
-  Book.fromMap(Map<String, dynamic> data) {
-    id = data['id'];
-    title = data['title'];
+  Book.fromMap(Map<String, dynamic> data) :
+    id = data['id'],
+    title = data['title'],
     year = data['year'];
-  }
 
   Map<String, dynamic> toMap() {
     return {
